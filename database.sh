bb44fd91ebcc3ce44a35abee2fc1919415dd4c80
Jack <jack.britton@yahoo.co.uk>
2024-12-17 16:03:38 +0000

    number 1

---
+++ database.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+# update
+sudo apt update -y
+
+# upgrade exclude debian
+sudo DEBIAN_FRONTEND=noninteractive apt upgrade -y
+
+# install mongoDB
+sudo DEBIAN_FRONTEND=noninteractive apt-get install gnupg curl
+
+# import MongoDB public GPG key
+curl -fsSL https://www.mongodb.org/static/pgp/server-7.0.asc | \
+    sudo gpg -o /usr/share/keyrings/mongodb-server-7.0.gpg \
+    --dearmor
+
+# create the list file
+echo "deb [ arch=amd64,arm64 signed-by=/usr/share/keyrings/mongodb-server-7.0.gpg ] https://repo.mongodb.org/apt/ubuntu jammy/mongodb-org/7.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-7.0.list
+
+# update for MongoDB
+sudo apt-get update -y
+
+# Install mongoDB Community server 7.0.6
+sudo DEBIAN_FRONTEND=noninteractive apt-get install -y mongodb-org=7.0.6 mongodb-org-database=7.0.6 mongodb-org-server=7.0.6 mongodb-mongosh mongodb-org-mongos=7.0.6 mongodb-org-tools=7.0.6
+
+# protects MongoDB version control
+echo "mongodb-org hold" | sudo dpkg --set-selections
+echo "mongodb-org-database hold" | sudo dpkg --set-selections
+echo "mongodb-org-server hold" | sudo dpkg --set-selections
+echo "mongodb-mongosh hold" | sudo dpkg --set-selections
+echo "mongodb-org-mongos hold" | sudo dpkg --set-selections
+echo "mongodb-org-tools hold" | sudo dpkg --set-selections
+
+# start MongoDB
+sudo systemctl start mongod
+
+# edit conf file ip
+sudo sed -i 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
+
+# restart MongoDB
+sudo systemctl restart mongod
+
+# enable MongoDB on startup
+sudo systemctl enable mongod
+
---
+++ sparta-app-script.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+
+# update
+sudo apt update -y
+
+# upgrade exclude debian
+sudo DEBIAN_FRONTEND=noninteractive apt upgrade -y
+
+# install nginx exclude debian
+sudo DEBIAN_FRONTEND=noninteractive apt-get install nginx -y
+
+# restart nginx
+sudo systemctl restart nginx
+
+# enable nginx
+sudo systemctl enable nginx
+
+# use curl to download the nodejs setup file
+curl -fsSL https://deb.nodesource.com/setup_20.x -o setup_nodejs.sh
+
+# run the setup file through bash
+sudo DEBIAN_FRONTEND=noninteractive bash setup_nodejs.sh
+
+# install nodejs
+sudo DEBIAN_FRONTEND=noninteractive apt install nodejs -y
+
+# install unzip
+sudo DEBIAN_FRONTEND=noninteractive apt install unzip -y
+
+# download sparta-app from github
+git clone https://github.com/Bag3ll/sparta-app.git
+
+# cd into sparta-app
+cd sparta-app
+
+# unzip folder
+unzip nodejs20-sparta-test-app.zip
+
+# set enviroment variables
+export DB_HOST=mongodb://10.0.3.4:27017/posts
+
+# make enviroment variable persist through restart
+echo 'export DB_HOST=mongodb://10.0.3.4:27017/posts' >> ~/.bashrc
+
+# go into app folder
+cd app
+
+# install app
+npm install
+
+#
+node seeds/seed.js
+
+# start app
+npm start &
+
+# show that the app is running
+echo "Checking if the app is running..."
+sleep 5
+if curl -s http://localhost:3000 > /dev/null; then
+    echo "App is running successfully on port 3000!"
+else
+    echo "Failed to start the app. Check logs for more details."
+fi
